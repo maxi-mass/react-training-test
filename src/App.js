79f48b752b96f13a0e42455bbf9a166b08fccee3
Maxim Svistunov <ma-xsoul@yandex.ru>
2020-05-08 09:07:15 +0300

    Modified app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navbar from './components/Navbar/Navbar';
 import Music from './components/Music/Music';
 import News from './components/News/News';
 import UsersContainer from "./components/Users/UsersContainer";
-
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from  "./components/Login/Login";
 import SettingsContainer from "./components/Settings/SettingsContainer";
