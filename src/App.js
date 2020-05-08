77479b46a51217da4703d89ee8c22cecd5379231
Maxim Svistunov <ma-xsoul@yandex.ru>
2020-05-08 09:09:45 +0300

    Removing unused import from app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Redirect, Route, withRouter} from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Music from './components/Music/Music';
 import News from './components/News/News';
